11521ed6cefec2893e71657511fb37768d5db148
Mhex12 <164562782+Mhex12@users.noreply.github.com>
2024-10-05 00:10:28 -0900

    Add files via upload

---
+++ Activity.php
@@ -0,0 +1,44 @@
+<?php 
+
+$firstname1 = "MAY";
+$lastname1 = "Flores";
+$age1 = "20";
+$Address1= "Antipolo";
+$Contact1 = "12344478909";
+
+$firstname2 = "Cams";
+$lastname2 = "Santos";
+$age2 = "50";
+$Address2 = "Antipolo";
+$Contact2 = "45243555898";
+
+$firstname3 = "PAUL";
+$lastname3 = "Santiago";
+$age3 = "34";
+$Address3 = "Antipolo";
+$Contact3 = "1234566909";
+
+$firstname4 = "ANNE";
+$lastname4 = "Cruz";
+$age4 = "28";
+$Address4 = "Antipolo";
+$Contact4 = "12345687909";
+
+?> 
+<DOCTYPE html>
+
+<html lang=en>
+
+<head>
+</head>
+
+<body>
+
+<p><?= $lastname1 ?></p>
+<p><?= $lastname2 ?></p>
+<p><?= $lastname3 ?></p>
+<p><?= $lastname4 ?></p>
+
+
+</body>
+</html>
